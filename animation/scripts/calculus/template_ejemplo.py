516117a37de15371ee7faae6814f6062ebb90f6d
Gerald C.A. <gerald.cainicela.a@gmail.com>
2025-04-10 16:21:03 -0500

    Add CompleteAnimation class for integral calculation with logo animation

---
+++ animation/scripts/calculus/template_ejemplo.py
@@ -0,0 +1,40 @@
+from manim import *
+from LnxScene import *
+
+# Clase principal de animación
+class CompleteAnimation(Scene):
+    def construct(self):
+        # Fondo personalizado (probablemente con color o imagen)
+        backgroundLnx(self)
+
+        # Establecer el template LaTeX con fuentes elegantes (mathpazo + kpfonts)
+        self.camera.tex_template = MathPazoKpTemplate()
+
+        box_anim = BoxAnimation( self, image_path="logo.png",  )
+        box_anim.on()
+        
+        # 2. Mover logo con ajustes
+        #logo_handler(  scene=self, image_path="logo.png" )
+        
+        # 4. El resto de tu lógica original
+        self.wait(2)
+        box_anim.off()
+        
+        #------------------------------------------
+ 
+        # TEXTO: Enunciado del problema
+        texto1 = MathTex( 
+            r"\text{Calcule la integral:} ",
+            font_size=22
+        )
+ 
+        # ANIMACIONES de entrada: Escritura tipo "dibujado"
+        self.play(Write(texto1),run_time=0.5)  # Escribir el enunciado
+        self.wait(0.5)  
+        #self.wait(1) 
+        animate_End( scene=self, svg_path="logo.svg" ) # finalizar con logo
+ 
+
+
+
+ 
