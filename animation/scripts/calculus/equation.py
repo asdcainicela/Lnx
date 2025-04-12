dc2c35672f2ad68bd26322534d7b848d8504ea78
Gerald C.A. <gerald.cainicela.a@gmail.com>
2025-04-12 12:59:14 -0500

    Remove unused grid function and adjust title color gradient in LinearEquation scene

--- animation/scripts/calculus/equation.py
+++ animation/scripts/calculus/equation.py
@@ -1,26 +1,12 @@
 from manim import *
-
-def grillado(scene):
-    # Crear la grilla
-    grid = NumberPlane(
-        x_range=[-4, 4, 0.3],  # Rango del eje X
-        y_range=[-6, 6, 0.3],    # Rango del eje Y
-        background_line_style={
-            "stroke_color": RED,  # Color de las líneas
-            "stroke_width": 0.5,     # Grosor de las líneas
-            "stroke_opacity": 0.1, # Opacidad de las líneas
-        },
-        axis_config={"stroke_opacity": 0},  # Ocultar los ejes
-    )
-    scene.add(grid)  # Agregar la grilla a la escena
-
-
+from LnxScene import *
+ 
 
 class LinearEquation(Scene):
     def construct(self):
         self.camera.background_color = "#111317" 
         title = Text("Resuelve", font_size=34).shift(UP * 3)
-        title.set_color_by_gradient(ORANGE, RED)  # Aplicar gradiente de colores
+        title.set_color_by_gradient(ORANGE, YELLOW)  # Aplicar gradiente de colores
         expr = MathTex(r" \frac{2x+5}{3} = 7 ", font_size=55).shift(UP * 1)
 
         question = MathTex(r" x=?", font_size=50).next_to(expr, DOWN, buff=1.3)  
