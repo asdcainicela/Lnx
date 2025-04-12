9f4b849eabea459bb1bb496faaf1ecaac76a61d6
Gerald C.A. <gerald.cainicela.a@gmail.com>
2025-04-12 12:22:37 -0500

    Replace question text with MathTex for consistency in LinearEquation scene

--- animation/scripts/calculus/equation.py
+++ animation/scripts/calculus/equation.py
@@ -6,7 +6,7 @@ class LinearEquation(Scene):
         self.camera.background_color = "#111317" 
         expr = MathTex(r" \frac{2x+5}{3} = 7 ", font_size=55).shift(UP * 0.5)
 
-        question = Tex("Solve for $x$", font_size=50).next_to(expr, DOWN, buff=1)
+        question = MathTex(r"x=?", font_size=50).next_to(expr, DOWN, buff=1)
 
         self.wait(1) 
         self.play(Write(expr))
