18745f363bf8f5c87c44c62736a5be3dc9053264
Gerald C.A. <gerald.cainicela.a@gmail.com>
2025-04-12 12:28:38 -0500

    Refactor animation sequence in LinearEquation scene to include a surrounding rectangle for the question text

--- animation/scripts/calculus/equation.py
+++ animation/scripts/calculus/equation.py
@@ -6,12 +6,23 @@ class LinearEquation(Scene):
         self.camera.background_color = "#111317" 
         expr = MathTex(r" \frac{2x+5}{3} = 7 ", font_size=55).shift(UP * 0.5)
 
-        question = MathTex(r"x=?", font_size=50).next_to(expr, DOWN, buff=1)
+        question = MathTex(r"x=?", font_size=50).next_to(expr, DOWN, buff=1) 
+        texts = VGroup(question).arrange(DOWN)
+        rect = SurroundingRectangle(texts, buff=0.5)
 
-        self.wait(1) 
         self.play(Write(expr))
-        self.play(Write(question))
-        self.wait(15)
+        self.wait(1) 
+        self.play(
+            Create(rect, run_time=3.0),
+            Succession(
+                Wait(1.0),
+                # You can Add a Mobject in the middle of an animation...
+                Add(question),
+                Wait(1.0),
+                # ...or multiple Mobjects at once! 
+            ),
+        ) 
+        self.wait(2)  
 
 
  
