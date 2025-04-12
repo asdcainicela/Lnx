227a3f8bd5658ed785bd091a2d6896c68186c45c
Gerald C.A. <gerald.cainicela.a@gmail.com>
2025-04-12 12:52:24 -0500

    Update grid line color to red and modify question text for clarity in LinearEquation scene

--- animation/scripts/calculus/equation.py
+++ animation/scripts/calculus/equation.py
@@ -6,7 +6,7 @@ def grillado(scene):
         x_range=[-4, 4, 0.3],  # Rango del eje X
         y_range=[-6, 6, 0.3],    # Rango del eje Y
         background_line_style={
-            "stroke_color": GREEN,  # Color de las líneas
+            "stroke_color": RED,  # Color de las líneas
             "stroke_width": 0.5,     # Grosor de las líneas
             "stroke_opacity": 0.1, # Opacidad de las líneas
         },
@@ -21,7 +21,7 @@ class LinearEquation(Scene):
         self.camera.background_color = "#111317" 
         expr = MathTex(r" \frac{2x+5}{3} = 7 ", font_size=55).shift(UP * 1)
 
-        question = MathTex(r"x=?", font_size=50).next_to(expr, DOWN, buff=1.3)  
+        question = MathTex(r"\text{Resuelve para} x=?", font_size=50).next_to(expr, DOWN, buff=1.3)  
 
         rect = SurroundingRectangle(
             question,
