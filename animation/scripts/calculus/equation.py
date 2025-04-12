a72d5ab654d32a71c19435112bad98b73d8d4082
Gerald C.A. <gerald.cainicela.a@gmail.com>
2025-04-12 12:55:08 -0500

    Adjust title position in LinearEquation scene for better alignment

--- animation/scripts/calculus/equation.py
+++ animation/scripts/calculus/equation.py
@@ -19,7 +19,7 @@ def grillado(scene):
 class LinearEquation(Scene):
     def construct(self):
         self.camera.background_color = "#111317" 
-        title = Text("Resuelve", font_size=50).shift(UP * 3.5).set_color([ORANGE,RED])
+        title = Text("Resuelve", font_size=50).shift(UP * 3).set_color([ORANGE,RED])
         expr = MathTex(r" \frac{2x+5}{3} = 7 ", font_size=55).shift(UP * 1)
 
         question = MathTex(r" x=?", font_size=50).next_to(expr, DOWN, buff=1.3)  
@@ -32,7 +32,8 @@ class LinearEquation(Scene):
             )
         
         grillado(self)
-
+        self.add(title)
+        self.wait(0.2)
         self.play(Write(expr))
         self.wait(1) 
         self.play(
