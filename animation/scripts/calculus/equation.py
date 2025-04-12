aaf57500dc226099a6cdafc004ff07d02274952d
Gerald C.A. <gerald.cainicela.a@gmail.com>
2025-04-12 12:30:25 -0500

    Refactor question text handling in LinearEquation scene to simplify rectangle creation

--- animation/scripts/calculus/equation.py
+++ animation/scripts/calculus/equation.py
@@ -6,9 +6,9 @@ class LinearEquation(Scene):
         self.camera.background_color = "#111317" 
         expr = MathTex(r" \frac{2x+5}{3} = 7 ", font_size=55).shift(UP * 0.5)
 
-        question = MathTex(r"x=?", font_size=50).next_to(expr, DOWN, buff=1) 
-        texts = VGroup(question).arrange(DOWN)
-        rect = SurroundingRectangle(texts, buff=0.5)
+        question = MathTex(r"x=?", font_size=50).next_to(expr, DOWN, buff=1)  
+
+        rect = SurroundingRectangle(question, buff=0.5)
 
         self.play(Write(expr))
         self.wait(1) 
