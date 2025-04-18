55940df46e97a2c857bdac46e64b61e494de0ed3
Gerald C.A. <gerald.cainicela.a@gmail.com>
2025-04-17 19:42:34 -0500

    Add SphereVolume class for 3D sphere visualization with animations and labels

--- animation/scripts/calculus/Shapes3D.py
+++ animation/scripts/calculus/Shapes3D.py
@@ -75,19 +75,55 @@ class CuboidVolume(ThreeDScene):
 		self.wait(2)
 		#SphereVolume.construct(self)
 
+
+class SphereVolume(ThreeDScene):
+	def construct(self): 
+  
+		title = MathTex(r"\text{Esfera}", font_size=35).shift(UP*2.7).set_color_by_gradient(ORANGE, YELLOW)
+
+		radius = MathTex("r", font_size=20, color=YELLOW).next_to(title, DOWN, buff=1.2)
+		area = MathTex(r"\pi", r"\cdot", "r^{2}", font_size=20, color=GOLD).next_to(title, DOWN, buff=1.2)
+		volume = MathTex(r"\frac{4}{3}", "\pi", "r^{3}", font_size=20, color=ORANGE).next_to(title, DOWN, buff=1.2)
+
+		label = [
+				MathTex("radio", font_size=20).next_to(radius,UP, buff=0.3),
+				MathTex("area", font_size=20).next_to(area, UP, buff=0.3),
+				MathTex("volumen", font_size=20).next_to(volume, UP, buff=0.3),
+		]
+
+		radiusValue=1
+
+		dot = Dot().shift([0, -0.5, 0])
+		line = Line([0, -0.5, 0], [radiusValue, -0.5, 0], color=BLUE_A)
+		circle = Circle(radius=radiusValue, color=BLUE).shift([0,  -0.5, 0])
+
+		sphere = Sphere(center=[0, -0.5, 0], radius=radiusValue, resolution=(10, 10)).rotate(40 * DEGREES, X_AXIS) #150,150
+
+		self.play(Create(title))
+		self.wait(0.5)
+		self.play(Create(dot))
+		self.play(Create(line), Write(radius), Write(label[0]))
+		self.wait(1)
+		self.play(Create(circle, run_time=1),
+		          Rotate(line, 360 * DEGREES, run_time=1, about_point=[0, -0.5, 0]),
+		          TransformMatchingTex(radius, area),
+		          TransformMatchingShapes(label[0], label[1]))
+		self.wait(1)
+		self.play(FadeOut(line, dot))
+		self.play(circle.animate.rotate(40 * DEGREES, X_AXIS))
+		self.wait(1)
+		self.play(Rotate(circle, 360 * DEGREES, axis=[0, 1, 1], run_time=1, rate_func=linear),
+		          Create(sphere, run_time=1))
+		self.play(TransformMatchingTex(area, volume),
+		          TransformMatchingShapes(label[1], label[2]))
+		self.wait(2)
+		self.play(FadeOut(VGroup(title, volume, label[2], sphere, circle)))
+		self.wait(2)
+	#CylinderVolume.construct(self)
+
 class CylinderVolume(ThreeDScene):
 	def construct(self):
-		rect_style = {
-							"stroke_width": 4,
-							"fill_opacity": 0
-					}
-   
-      # Crear rectángulos con parámetros específicos
-		rectangles = VGroup(
-            Rectangle(width=4, height=7.6, color=YELLOW, **rect_style),
-            Rectangle(width=3.5, height=6, color=GREEN, **rect_style)
-        )
-		self.add(rectangles)
+		 
   
 		title = MathTex(r"\text{Cilindro}", font_size=35).shift(UP*2.7).set_color_by_gradient(BLUE_A,   BLUE)
 
@@ -134,4 +170,3 @@ class CylinderVolume(ThreeDScene):
 		self.wait(2)
 		self.play(FadeOut(VGroup(title, volume, label[2], cylinder, circle)))
 		self.wait(2)
-		
